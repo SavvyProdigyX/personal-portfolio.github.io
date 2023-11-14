15749cf8ce22a58ebd1e60eca65e565eb4983fff
denyfaishal25 <denyfaishalardiyan@gmail.com>
2023-11-14 23:42:41 +0700

    ADDING JS FOR UTS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function() {
-    header.classList.toggle ("sticky", window.scrollY> 90);
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 90);
 });
-
-const tamu = prompt('Siapakah Anda?');
-alert('Selamat datang, ' + tamu + '!');
-
